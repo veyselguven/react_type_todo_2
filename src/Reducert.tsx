507f38456f845bd3d6ceda2924b2f0dc4554a5ce
veyselguven <vgbasbaydar@gmail.com>
2023-07-01 11:40:17 -0700

    added some useReducer

---
+++ src/Reducert.tsx
@@ -0,0 +1,32 @@
+// import React from "react";
+
+// export interface Todo {
+//   id: number;
+//   todo: string;
+//   isDone: boolean;
+// }
+
+// type Actions =
+//   | { type: "add"; payload: string }
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number };
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//   switch (action.type) {
+//     case "add":
+//       return [
+//         ...state,
+//         { id: Date.now(), todo: action.payload, isDone: false },
+//       ];
+//     case "remove":
+//       return state.filter((todo) => todo.id !== action.payload);
+//   case "done":
+//   return state.map((todo)=> todo.id !== action.payload ? {...todo,isDone: !todo.isDone} :todo);
+//   default:
+//     return state
+// };
+// import { useReducer } from "react";
+// const Reducert = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+//   return<div>efg</div>
+// };
